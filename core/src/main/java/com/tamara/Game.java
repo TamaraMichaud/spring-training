0237983ae798ef5be9847155e1c85f5829dd4502
Tamara.Michaud <tamara.michaud@smartfocus.com>
2020-01-30 10:04:10 +0000

    add: created Game interface & implementation

---
+++ core/src/main/java/com/tamara/Game.java
@@ -0,0 +1,16 @@
+package com.tamara;
+
+public interface Game {
+
+    int getNumber();
+    int getGuess();
+    void setGuess(int guess);
+    int getSmallest();
+    int getBiggest();
+    int getRemaining();
+    void reset();
+    void check();
+    boolean isValidNumberRange();
+    boolean isGameWon();
+    boolean isGameLost();
+}
---
+++ core/src/main/java/com/tamara/GameImpl.java
@@ -0,0 +1,105 @@
+package com.tamara;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GameImpl implements Game {
+
+    // constants
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameImpl.class);
+
+    // fields
+    private NumberGenerator numberGenerator;
+    private int guessCount = 10;
+    private int number;
+    private int guess;
+    private int smallest;
+    private int biggest;
+    private int remainingGuesses;
+    private boolean validNumberRange = true;
+
+
+    // private methods
+    private void checkValidNumberRange(){
+        this.validNumberRange = (this.guess >= this.smallest) && (this.guess <= this.biggest);
+    }
+
+
+    // public methods
+
+    @Override
+    public void reset() {
+        this.smallest = 0;
+        this.guess = 0;
+        this.remainingGuesses = guessCount;
+        this.biggest = numberGenerator.getMaxNumber();
+        this.number = numberGenerator.next();
+        LOGGER.debug("Number chosen is {}", this.number);
+    }
+    // ^^ START / RESTART the game
+
+
+    @Override
+    public void check() {
+        checkValidNumberRange();
+        if(validNumberRange) {
+            if(guess > number) {
+                this.biggest = guess - 1; // << we have guessed too high... so we block any guesses that are equal to or higher.
+            }
+            if(guess < number) {
+                this.smallest = guess + 1; //
+            }
+        }
+        remainingGuesses--;
+
+    }
+
+
+
+
+
+    @Override
+    public int getNumber() {
+        return this.number;
+    }
+
+    @Override
+    public int getGuess() {
+        return this.guess;
+    }
+
+    @Override
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
+
+    @Override
+    public int getSmallest() {
+        return this.smallest;
+    }
+
+    @Override
+    public int getBiggest() {
+        return this.biggest;
+    }
+
+    @Override
+    public int getRemaining() {
+        return this.remainingGuesses;
+    }
+
+    @Override
+    public boolean isValidNumberRange() {
+        return this.validNumberRange;
+    }
+
+    @Override
+    public boolean isGameWon() {
+        return this.guess == this.number;
+    }
+
+    @Override
+    public boolean isGameLost() {
+        return !isGameWon() && remainingGuesses <= 0;
+    }
+}
