5ae840cf9b3fe4d73a690de232ae94708064beff
Tamara.Michaud <tamara.michaud@smartfocus.com>
2020-02-05 14:08:06 +0000

    add: class to handle ApplicationEvent

--- core/src/main/java/com/tamara/AppConfig.java
+++ core/src/main/java/com/tamara/AppConfig.java
@@ -4,10 +4,11 @@ package com.tamara;
 import com.tamara.messageGeneratorChallenge.MessageGenerator;
 import com.tamara.messageGeneratorChallenge.MessageGeneratorImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan(basePackages = "com.tamara")
+@ComponentScan(basePackages = "com.tamara")
 public class AppConfig {
 
     //   -- bean methods --  (by default, beans are named the same as their class... you can override this but not recommended)
