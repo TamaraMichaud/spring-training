076342eea07ff3170537d20fd2e9d14a9a9042f0
TamaraMichaud <tamaramichaud@hotmail.com>
2020-02-17 12:50:50 +0000

    generate and load spring boot project from Spring Initializr site (all automatic code so far...)

---
+++ spring-boot-training-1/src/test/java/com/tamara/springboottraining1/SpringBootTraining1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamara.springboottraining1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTraining1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
